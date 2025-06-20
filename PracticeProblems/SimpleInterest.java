2372403f3aeeb312331e5e534f971c9ae6e78099
Abhishek Sharma <abhishek.sharma3_cs22@gla.ac.in>
2025-06-20 10:38:28 +0530

    [Namit Singh].Add Practice Problems Java Day 1

---
+++ PracticeProblems/SimpleInterest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+class SimpleInterest
+{
+    public static void main(String[]args){
+
+    Scanner sc = new Scanner(System.in);
+
+    double principal = sc.nextDouble();
+    double rate = sc.nextDouble();
+    double time = sc.nextDouble();
+    double simple_interest = (principal * rate * time) / 100;  
+    System.out.println(simple_interest);
+   }
+}
---
+++ PracticeProblems/welcome.java
@@ -0,0 +1,8 @@
+class Bridgelabz
+
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
