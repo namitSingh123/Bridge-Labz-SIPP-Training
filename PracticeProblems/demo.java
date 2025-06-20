b2dc2dbc9de3369d6db7ecab915747b7f8a11cb8
Namit Singh <thakurnamit066@gmail.com>
2025-06-20 10:50:39 +0530

    nignRevert "Changing username"
    anignRevert "[Namit Singh].Add Practice Problems Java Day 1"
    
    anignRevert "[Namit Singh].Add Practice Problems Java Day 1"
    
    anignRevert "[Namit Singh].Add Practice Problems Java Day 1"
    
    This reverts commit 2372403f3aeeb312331e5e534f971c9ae6e78099.

--- PracticeProblems/demo.java
+++
@@ -1,8 +0,0 @@
-class Demo
-
-{
-    public static void main (String[] args)
-    {
-        System.out.println("Welcome to Bridgelabz!);
-    }
-}
--- PracticeProblems/welcome.java
+++
@@ -1,8 +0,0 @@
-class Bridgelabz
-
-{
-    public static void main (String[] args)
-    {
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
